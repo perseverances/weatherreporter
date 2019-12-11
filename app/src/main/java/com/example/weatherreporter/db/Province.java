925b9ca4b546743d33dc4dd496bf228a72a2a981
perseveances <tao.best@foxmail.com>
2019-12-11 11:31:16 +0800

    2th commit

---
+++ app/src/main/java/com/example/weatherreporter/db/Province.java
@@ -0,0 +1,29 @@
+package com.example.weatherreporter.db;
+
+public class Province extends DataSupport{
+    private int id;
+    private String provinceName;
+    private int provinceCode;
+
+    private int getId(){
+        return id;
+    }
+    public void setId(int id){
+        this.id = id;
+    }
+
+    public String getProvinceName(){
+        return provinceName;
+    }
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+
+    public int getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setProvinceCode(int provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+}
